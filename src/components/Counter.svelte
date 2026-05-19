702bc4ed1e35c7e31152e33f47331a9f5744859d
Eugene Lebedev <eugene.0x90@gmail.com>
2026-05-19 10:55:41 +0300

    deploy: a555266

--- src/components/Counter.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  let { initial = 0 }: { initial?: number } = $props();
-  let count = $state(initial);
-</script>
-
-<button type="button" onclick={() => count++}>
-  Clicks: {count}
-</button>
